8c8d37ed1619522c3dab29b60486f44232a37adb
M Harsh <zesterschmid16@gmail.com>
2024-02-11 16:54:42 +0530

    03_reverse_int/done/harsh

--- exercises_m/03_reverseint/index.js
+++ exercises_m/03_reverseint/index.js
@@ -8,6 +8,38 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
 
+	return callbackInt(n)
+
+}
+
+/**
+ * Harsh Solution with recursion 
+ * @param {*} num 
+ * @returns 
+ */
+function callbackInt(num) {
+
+	if (num < 0) {
+		return callbackPositiveInt(num * -1, 0) * -1
+	}
+	return (callbackPositiveInt(num, 0))
+
+
+}
+
+function callbackPositiveInt(num, sum) {
+
+	sum = (num % 10) + (sum * 10)
+	if (num < 10) {
+		return sum
+	}
+	const quotient = Math.floor(num / 10)
+	return callbackPositiveInt(quotient, sum)
+
+
+}
+
+console.log(reverseInt(1201))
 module.exports = reverseInt;
