6c7eeb5931b15081ab978a688a905854a36a0f08
M Harsh <zesterschmid16@gmail.com>
2024-02-24 21:54:57 +0530

    08_capitalize/done

--- exercises_m/08_capitalize/index.js
+++ exercises_m/08_capitalize/index.js
@@ -7,6 +7,18 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+	return arrayStringFunc(str)
+}
 
+function arrayStringFunc(str) {
+
+	return str.split(" ").map(element => {
+		const y = element[0].toUpperCase() + String(element.length > 1 ? element.slice(1) : "")
+		return y
+	}).join(" ")
+}
+
+//console.log(capitalize('a short sentence'))
+//console.log(capitalize('look, it is working!'))
 module.exports = capitalize;
