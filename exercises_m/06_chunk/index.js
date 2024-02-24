52326a829f46f70eb1ae3455d15d589bde2a07ca
M Harsh <zesterschmid16@gmail.com>
2024-02-24 17:54:05 +0530

    06_chunk/done/generalSolution

--- exercises_m/06_chunk/index.js
+++ exercises_m/06_chunk/index.js
@@ -9,9 +9,12 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-	return chunkHarshImpl(array, size)
+	//return chunkHarshImpl(array, size)
+	return chunkArrayGenericImpl(array, size)
 }
 
+//slice method
+//chunk array by checking length. if input length is less than said sixe 
 function chunkHarshImpl(array, size) {
 
 	if (array.length <= size) {
@@ -28,7 +31,31 @@ function chunkHarshImpl(array, size) {
 	//return array.slice(0, size)
 	return returnArray
 }
+
+function chunkArrayGenericImpl(array, size) {
+
+	const chunkedArray = []
+	// for (ele of array) {
+	// 	let subArray = []
+	// 	subArray.push(ele)
+	// 	if (subArray.size == size) {
+	// 		chunkedArray.push(subArray)
+	// 	}
+	//}
+	let subArray = []
+	array.forEach((ele, pos) => {
+
+		subArray.push(ele)
+		if (subArray.length == size || pos == (array.length - 1)) {
+			chunkedArray.push(subArray)
+			subArray = []
+		}
+
+	});
+
+	return chunkedArray
+}
 //console.log(chunk([1, 2, 3, 4, 5], 1))
 //console.log(chunk([1, 2, 3, 4], 6))
-
+//console.log(chunkArrayGenericImpl([1, 2, 3, 4], 6))
 module.exports = chunk;
