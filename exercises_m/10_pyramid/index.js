fb5e0e854aa30ec75a843e30d77d44755801304e
M Harsh <zesterschmid16@gmail.com>
2024-03-10 15:30:42 +0530

    10_pyramid/harsh/done

--- exercises_m/10_pyramid/index.js
+++ exercises_m/10_pyramid/index.js
@@ -14,6 +14,54 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) { setPyramid(n) }
 
+
+/**
+ * My Implemetation
+ * @param {*} n 
+ * @param {*} i 
+ * @param {*} str 
+ */
+function setPyramid(n, tri = 0, i = 1) {
+	if (tri === 0) {
+		tri = (2 * n) - 1
+	}
+
+
+	const steps = (2 * i) - 1
+	const space = (tri - steps) / 2
+
+	//console.log(tri, steps, space)
+	const strSteps = setSteps(steps)
+	const strSpace = setSpace(space)
+
+	console.log(strSpace + strSteps + strSpace)
+	if (i === n) {
+		return
+	}
+	setPyramid(n, tri, i + 1)
+
+}
+
+
+function setSteps(steps, i = 0, str = "") {
+	if (i === steps) {
+		return str
+	}
+
+	str += "#"
+	return setSteps(steps, i + 1, str)
+
+}
+
+function setSpace(spaces, i = 0, str = "") {
+	if (i === spaces) {
+		return str
+	}
+
+	str += " "
+	return setSpace(spaces, i + 1, str)
+}
+pyramid(6)
 module.exports = pyramid;
