ce0e8dce09e19236e1cc342d0f20a6fee9cc1c8a
M Harsh <zesterschmid16@gmail.com>
2024-03-10 19:02:54 +0530

    11_vowels/done/loop

--- exercises_m/11_vowels/index.js
+++ exercises_m/11_vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) { return hIterationImpl(str) }
 
+
+function hIterationImpl(str) {
+	const st = str.toLowerCase()
+	let counter = 0
+	for (let e of st) {
+		if (e === "a" || e === "e" || e === "i" || e === "o" || e === "u") {
+			counter++
+		}
+	};
+	return counter
+}
+
+console.log(vowels('Hi There!'))
+console.log(vowels('Why do yOu Ask?'))
 module.exports = vowels;
