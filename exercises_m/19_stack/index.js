72898806323e37acce4490706ee77dff5734bb26
M Harsh <zesterschmid16@gmail.com>
2024-05-26 18:20:48 +0530

    19_stack/impl/completed

--- exercises_m/19_stack/index.js
+++ exercises_m/19_stack/index.js
@@ -10,6 +10,38 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack { }
+class Stack {
+	constructor() {
+		this.array = []
+	}
 
+	push(element) {
+		if (!element) {
+			throw new Error("Please pass the element")
+		}
+
+		this.array.push(element)
+	}
+
+
+	pop() {
+		return this.array.pop()
+	}
+
+	peek() {
+		return this.array[this.array.length - 1]
+
+	}
+}
+
+function testStack() {
+	const stc = new Stack()
+	stc.push(1)
+	stc.push('a')
+	console.log(stc.array)
+	console.log(stc.pop())
+	console.log(stc.pop())
+
+}
+testStack()
 module.exports = Stack;
