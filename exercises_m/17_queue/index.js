f823a7ed45aa075fa934bf09ffb2c4eab75fc6ae
M Harsh <zesterschmid16@gmail.com>
2024-05-26 15:51:56 +0530

    17_queue/impl/completed

--- exercises_m/17_queue/index.js
+++ exercises_m/17_queue/index.js
@@ -8,6 +8,43 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+
+
+	constructor() {
+		this.array = []
+	}
+
+
+
+	add(element) {
+		if (element === undefined) {
+			throw new Error("element should not be empty")
+		}
+		this.array.unshift(element)
+		console.log(`element: ${element} added. current queue:${this.array}`)
+	}
+
+	remove() {
+		if (this.array.length <= 0) {
+			//throw new Error("There is nothing to remove")
+			console.log(`element removed current queue after pop:${this.array}`)
+			return undefined
+		}
+
+		const elementRemoved = this.array.pop()
+		console.log(`element ${elementRemoved} removed current queue after pop:${this.array}`)
+		return elementRemoved
+	}
+
+
+}
+
+const queue = new Queue()
+//queue.add(1)
+//queue.add(2)
+//queue.remove()
+//queue.remove()
+//queue.remove()
 
 module.exports = Queue;
