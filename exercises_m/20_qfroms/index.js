92e044d6009198989fde28f767760d444e287b04
M Harsh <zesterschmid16@gmail.com>
2024-05-26 20:26:59 +0530

    20_qfroms/impl/completed

--- exercises_m/20_qfroms/index.js
+++ exercises_m/20_qfroms/index.js
@@ -14,6 +14,75 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+	constructor() {
+		this.stackMain = new Stack()
+		this.stackBackup = new Stack()
+		this.firstElement
+	}
 
+
+	add(element) {
+		if (!this.stackMain.peek()) {
+			this.firstElement = element
+		}
+
+		this.stackMain.push(element)
+
+	}
+
+	remove() {
+		console.log("into remove")
+		if (!this.stackMain.peek()) {
+			this.firstElement = undefined
+			return undefined
+		}
+		let lastElement;
+		let counter = 0;
+		while (this.stackMain.peek()) {
+			console.log("into pop elements from original stack")
+			lastElement = this.stackMain.pop()
+			this.stackBackup.push(lastElement)
+		}
+		console.log(`backup:${this.stackBackup.data}, main:${this.stackMain.data}`)
+		while (this.stackBackup.peek()) {
+			let newElement = this.stackBackup.pop()
+			console.log("into push elements from backup stack")
+
+			if (counter === 1) {
+				this.firstElement = newElement
+				counter++
+			}
+			if (newElement !== lastElement) {
+				this.stackMain.push(newElement)
+			}
+			else {
+				counter = 1;
+			}
+		}
+
+		return lastElement;
+
+	}
+
+	peek() {
+		return this.firstElement
+	}
+
+}
+function testQ() {
+	const q = new Queue();
+	q.add(1)
+	console.log(q.peek())
+	q.add(2)
+	q.add("hi")
+	q.add("jujima")
+	console.log(q.peek())
+	console.log(q.remove())
+	console.log(q.remove())
+	console.log(q.remove())
+	console.log(q.peek())
+}
+
+testQ()
 module.exports = Queue;
