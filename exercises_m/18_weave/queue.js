29ea3668ebd7b0e8db1b0e682bf34c05c544309f
M Harsh <zesterschmid16@gmail.com>
2024-05-26 16:44:47 +0530

    17_weave/impl/complete queue peek

--- exercises_m/18_weave/queue.js
+++ exercises_m/18_weave/queue.js
@@ -16,6 +16,17 @@ class Queue {
   remove() {
     return this.data.pop();
   }
+
+  peek() {
+    return this.data[this.data.length - 1]
+  }
 }
+
+const q = new Queue()
+
+//q.add(1)
+//q.add(10)
+//q.add(5)
+console.log(q.peek())
 
 module.exports = Queue;
