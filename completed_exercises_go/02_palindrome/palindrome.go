b92260449451dcfe17096416fba130e2934d799e
M Harsh <=>
2024-06-17 15:49:22 +0530

    add golang palindrome tests

--- completed_exercises_go/02_palindrome/palindrome.go
+++ completed_exercises_go/02_palindrome/palindrome.go
@@ -7,10 +7,6 @@ func Palindrome(input string) bool {
 
 	for pos, _ := range input {
 		revPos := length - pos - 1
-		//fmt.Println(input)
-		//fmt.Println(input[revPos])
-		//fmt.Println(val)
-
 		if input[revPos] == input[pos] {
 			status++
 		}
