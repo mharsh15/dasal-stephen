14d1938c46ee2d868a551ab6296ec1500e5c970a
M Harsh <zesterschmid16@gmail.com>
2024-07-07 18:32:29 +0530

    25_levelwidth/impl/implement depth

--- exercises_m/26_levelwidth/index.js
+++ exercises_m/26_levelwidth/index.js
@@ -10,7 +10,39 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
+const Node = require('./node');
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	const counter = [];
+	let children = [root]
+	while (children.length > 0) {
+		counter.push(children.length)
+		const newChildren = []
+		for (let child of children) {
+			newChildren.push(...child.children)
+		}
+		children = newChildren
 
+	}
+	return counter
+}
+
+function testLevelWidth() {
+	const root = new Node(0);
+	root.add(1);
+	root.children[0].add(2);
+	root.children[0].add(3);
+	root.children[0].children[0].add(4);
+	console.log("R: ", levelWidth(root))
+
+	const root2 = new Node(0);
+	root2.add(1);
+	root2.add(2);
+	root2.add(3);
+	root2.children[0].add(4);
+	root2.children[2].add(5);
+	console.log("R2: ", levelWidth(root2))
+	//expect(levelWidth(root)).toEqual([1, 1, 2, 1]);
+}
+testLevelWidth()
 module.exports = levelWidth;
