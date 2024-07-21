f09870a3effd4370358a9e20f887d60003bd515b
M Harsh <zesterschmid16@gmail.com>
2024-07-21 07:10:04 +0530

    28_validate/impl/implemvalidate

--- exercises_m/28_validate/index.js
+++ exercises_m/28_validate/index.js
@@ -5,6 +5,94 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+const Node = require('./node');
 
+function validate(node, min = null, max = null) {
+	let leftStatus
+	let rightStatus
+
+	if (!node) {
+		return true
+	}
+	//console.log(node.data, min, max)
+
+
+
+
+	if (!min && !max) {
+		leftStatus = validate(node.left, null, node.data)
+		rightStatus = validate(node.right, node.data, null)
+		console.log("final result", " left:", leftStatus, " right:", rightStatus)
+		return leftStatus && rightStatus
+	}
+
+	if (!node.left) {
+		leftStatus = true
+	}
+	else if (node.left.data < node.data) {
+		leftStatus = validate(node.left, min, max)
+
+	} else {
+		leftStatus = false
+	}
+
+	if (!node.right) {
+		rightStatus = true
+	}
+	else if (node.right.data > node.data) {
+		console.log("too big max nr:", node.right.data, max)
+		if (max && node.right.data > max) {
+			rightStatus = false
+
+		}
+		else {
+
+			rightStatus = validate(node.right, min, max)
+		}
+	} else {
+		rightStatus = false
+	}
+
+
+	//console.log(node.data, leftStatus, rightStatus,)
+	return leftStatus && rightStatus
+
+
+
+}
+
+function testOne() {
+	const n = new Node(10);
+	n.insert(5);
+	n.insert(15);
+	n.insert(0);
+	n.insert(20);
+	console.log(n)
+	console.log(validate(n))
+}
+
+function testTwo() {
+	const n = new Node(10);
+	n.insert(5);
+	n.insert(15);
+	n.insert(0);
+	n.insert(20);
+	n.left.right = new Node(-10)
+	console.log(n)
+	console.log(validate(n))
+}
+
+function testThree() {
+	const n = new Node(10);
+	n.insert(5);
+	n.insert(15);
+	n.insert(0);
+	n.insert(20);
+	n.left.left.right = new Node(999);
+	//console.log(n)
+	console.log(validate(n))
+}
+//testOne() //true
+//testTwo() //false
+testThree() //false
 module.exports = validate;
