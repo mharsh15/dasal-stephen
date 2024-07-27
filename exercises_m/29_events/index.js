d9ebd295020c1bd0e3016c7572aecd620f354c6a
M Harsh <zesterschmid16@gmail.com>
2024-07-27 19:32:38 +0530

    29_events/impl/implementEvents

--- exercises_m/29_events/index.js
+++ exercises_m/29_events/index.js
@@ -5,15 +5,55 @@
 
 class Events {
   // Register an event handler
-  on(eventName, callback) {}
+  eventList = []
+  on(eventName, callback) {
+    this.eventList.push({ event: eventName, callback })
+    //console.log(this.eventList)
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    const eventList = this.eventList.filter((event) => {
+      return event.event === eventName
+    })
+
+    console.log(eventList)
+    eventList.forEach((event) => {
+      event.callback()
+    })
+
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    const excludeEventArray = this.eventList.filter((event) => {
+      return event.event !== eventName
+    })
+
+    this.eventList = excludeEventArray
+  }
 }
+
+
+function testOne() {
+  const events = new Events();
+
+  const cb1 = () => {
+    console.log("clicked 1")
+  }
+  const cb2 = () => {
+    console.log("hover 2")
+  }
+
+  events.on('click', cb1);
+  events.trigger('click');
+  events.on('hover', cb2);
+  events.trigger('click');
+  events.trigger('hover');
+
+}
+//testOne()
 
 module.exports = Events;
