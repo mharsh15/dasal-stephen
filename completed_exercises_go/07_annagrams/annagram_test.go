cef686ce3e22c7a23cfe2ab518d215658d0a74dc
M Harsh <=>
2024-11-27 20:22:22 +0530

    07_annagram/go/impl/ add test cases

--- completed_exercises_go/07_annagrams/annagram_test.go
+++ completed_exercises_go/07_annagrams/annagram_test.go
@@ -2,6 +2,28 @@ package main
 
 import "testing"
 
-func Test_Annagram(t *testing.T) {
+//	anagrams('rail safety', 'fairy tales') --> True
+//	anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//	anagrams('Hi there', 'Bye there') --> False
+func Test_Annagram_Normal(t *testing.T) {
+	resp := annagram("rail safety", "fairy tales")
 
+	if !resp {
+		t.Error("for (rail safety, fairy tales) result is not true")
+	}
+}
+func Test_Annagram_Capital(t *testing.T) {
+	resp := annagram("RAIL! SAFETY!", "fairy tales")
+
+	if !resp {
+		t.Error("for (RAIL! SAFETY!,) result is not true")
+	}
+}
+
+func TestFalse(t *testing.T) {
+	resp := annagram("Hi there", "Bye there")
+
+	if resp {
+		t.Error("for (Hi there", "Bye there) result is not true")
+	}
 }
